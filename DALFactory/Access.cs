d580b23dcfef7b3678c0a51c6ee6a3422f14d026
FrankFan <fanyong@gmail.com>
2014-04-19 00:11:15 +0800

    commit

---
+++ DALFactory/Access.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using IDAL;
+
+namespace DALFactory
+{
+    public class Access
+    {
+        public static IOrder CreateOrder()
+        {
+            SQLServerDAL.Order order = new SQLServerDAL.Order();
+            return (IOrder)order;
+        }
+    }
+}
